b0f57d8e948322b1cb9300a8849d5791af63a9cc
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2024-12-02 15:35:54 +0100

    Added solution for Exercise20

--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -2,10 +2,15 @@ package com.campusdual.classroom;
 
 public class PoliceOfficer extends Person {
 
-	protected String squad;
+  protected String squad;
 
-	public PoliceOfficer(String name, String surname, String squad) {
-		super(name, surname);
-		this.squad = squad;
-	}
+  public PoliceOfficer(String name, String surname, String squad) {
+    super(name, surname);
+    this.squad = squad;
+  }
+
+  @Override
+  public void getDetails() {
+    System.out.println("Doctor " + name + " " + surname + ", del escuadrón " + squad.toLowerCase());
+  }
 }
